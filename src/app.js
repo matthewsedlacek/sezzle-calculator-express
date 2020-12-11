71c7421ecb1e2f4dc6326c34b015ce1235a67a00
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-12-10 18:51:01 -0800

    changes for heroku

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const http = require("http");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
--- src/ioService.js
+++ src/ioService.js
@@ -8,7 +8,6 @@ const ioService = {
 
     io.on("connection", function (socket) {
       const db = app.get("db");
-      // socket.emit('logs', logsService.getLogs());
       socket.on("new_calculation", function (calculation) {
         logsService.insertLog(db, calculation);
         io.sockets.emit("new_calculation_blast", calculation);
